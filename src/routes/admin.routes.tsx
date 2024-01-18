6087af0130d81fba6e3990cbc390b58906981fb8
oliullah05 <mdolihasan@gmail.com>
2024-01-18 18:49:59 +0600

    module 6 stater pack

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,8 +1,8 @@
 import { NavLink } from "react-router-dom";
-import AdminDashboard from "./admin/AdminDashboard";
-import CreateAdmin from "./admin/CreateAdmin";
-import CreateFaculty from "./admin/CreateFaculty";
-import CreateStudent from "./admin/CreateStudent";
+import AdminDashboard from "../pages/admin/AdminDashboard";
+import CreateAdmin from "../pages/admin/CreateAdmin";
+import CreateFaculty from "../pages/admin/CreateFaculty";
+import CreateStudent from "../pages/admin/CreateStudent";
 import { ReactNode } from "react";
 
 
@@ -65,11 +65,38 @@ export const adminRoutes = adminPaths.reduce((acc:TRoute[], item) => {
 }, [])
 
 
-console.log(adminRoutes);
 
 
 
 
+type TAdminSidebarItem ={
+  
+  key:string,
+  label:ReactNode,
+  children?:TAdminSidebarItem[]
+}
+
+
+export const adminSidebarItems = adminPaths.reduce((acc:TAdminSidebarItem[], item) => {
+  if (item.path && item.name) {
+      acc.push({
+          key: item.name,
+          label:<NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>
+      })
+  }
+
+  if(item.children){
+              acc.push({
+                  key: item.name,
+                  label:item.name,
+                  children:item.children.map(child=>({
+                      key:child.name,
+                      label:<NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>
+                  }))
+              })
+          }
+  return acc
+}, [])
 
 
 
@@ -111,32 +138,3 @@ console.log(adminRoutes);
 
 
 
-
-type TAdminSidebarItem ={
-  
-  key:string,
-  label:ReactNode,
-  children?:TAdminSidebarItem[]
-}
-
-
-export const adminSidebarItems = adminPaths.reduce((acc:TAdminSidebarItem[], item) => {
-  if (item.path && item.name) {
-      acc.push({
-          key: item.name,
-          label:<NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>
-      })
-  }
-
-  if(item.children){
-              acc.push({
-                  key: item.name,
-                  label:item.name,
-                  children:item.children.map(child=>({
-                      key:child.name,
-                      label:<NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>
-                  }))
-              })
-          }
-  return acc
-}, [])
