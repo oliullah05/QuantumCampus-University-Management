f32260b58e1e377a30bcdf8f2fc1a873608809ed
oliullah05 <mdolihasan@gmail.com>
2024-01-20 21:35:56 +0600

    Requirement Analysis For Authentication And Backend Setup

--- src/components/layouts/SideBar.tsx
+++ src/components/layouts/SideBar.tsx
@@ -26,6 +26,7 @@ const SideBar = () => {
             sidebarItems = FacultyItems
             break;
 
+            
         case useRole.STUDENT:
             sidebarItems = Studenttems
             break;
