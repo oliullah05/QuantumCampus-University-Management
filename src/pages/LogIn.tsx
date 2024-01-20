c459460ed0f7a4ba62cae778090988987e74a17f
oliullah05 <mdolihasan@gmail.com>
2024-01-20 22:50:36 +0600

    Basic Login Form And UseLoginMutaion

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,9 +1,41 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useAppDispatch } from "../redux/hooks";
+import { useLogInMutation } from "../redux/features/auth/authApi";
 
 const LogIn = () => {
+    const { register, handleSubmit } = useForm({
+        defaultValues:{
+            userId:"A-0001",
+            password:"admin123"
+        }
+    })
+
+    const [login, { data, error }] = useLogInMutation()
+    console.log({error,data});
+    const onSubmit = (data) => {
+        const userInfo = {
+            id:data.userId,
+            password:data.password
+        }
+        console.log(userInfo);
+        login(userInfo)
+    }
+
+
+
+
+
+
+
     return (
-        <div>
-            LOg in
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="id">ID:</label>
+            <input type="text" id="id" {...register("userId")} />
+            <label htmlFor="password">ID:</label>
+            <input type="password" id="password" {...register("password")} />
+            <Button htmlType="submit">LogIn</Button>
+        </form>
     );
 };
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
+import { baseApi } from "./api/baseApi";
 export const store = configureStore({
    reducer:{
       auth:authReducer
    },
+   [baseApi.reducerPath]:[baseApi.reducer]
 
 })
 
